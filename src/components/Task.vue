547876194aff193e9ffcc8e7835950ddb216330c
Kazuki Aoshima <aocm.dev@gmail.com>
2021-03-03 03:49:11 +0900

    写経ミス対応＋composite-component: Automated Testing
    ミスったことはこちらにメモ https://zenn.dev/aocm/scraps/3ae7ea5a0c0c68#comment-e45c501a4ef429

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,8 +1,20 @@
 <!-- src/components/Task.vue -->
 
 <template>
-  <div class="list-item">
-    <input type="text" readonly :value="task.title" />
+  <div class="list-item" :class="task.state">
+    <label class="checkbox">
+      <input type="checkbox" :checked="isChecked" disabled name="checked" />
+      <span class="checkbox-custom" @click="$emit('archive-task', task.id)" />
+    </label>
+    <div class="title">
+      <input type="text" :value="task.title" readonly placeholder="Input title" />
+    </div>
+
+    <div class="actions">
+      <a v-if="!isChecked" @click="$emit('pin-task', task.id)">
+        <span class="icon-star" />
+      </a>
+    </div>
   </div>
 </template>
 
@@ -15,6 +27,11 @@
         required: true,
         default: () => ({ id: '', state: '', title: '' }),
         validator: task => ['id', 'state', 'title'].every(key => key in task),
+      },
+    },
+    computed: {
+      isChecked() {
+        return this.task.state === 'TASK_ARCHIVED';
       },
     },
   };
