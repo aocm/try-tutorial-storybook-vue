eed1e38912c1e0351366ccb42bf7703f2333b23f
Kazuki Aoshima <aocm.dev@gmail.com>
2021-03-05 07:19:15 +0900

    screen: Nested container components
    https://storybook.js.org/tutorials/intro-to-storybook/vue/en/screen/

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,19 @@
 
 <template>
   <div id="app">
-    <task-list />
+    <InboxScreen />
   </div>
 </template>
 
 <script>
   import store from './store';
-  import TaskList from './components/TaskList.vue';
+  import InboxScreen from './components/InboxScreen.vue';
 
   export default {
     name: 'app',
     store,
     components: {
-      TaskList,
+      InboxScreen,
     },
   };
 </script>
